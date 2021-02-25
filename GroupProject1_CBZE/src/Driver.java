b8589cfd9c5011e1c3991ce39618348f37fd58b0
CBolduc10 <44456874+CBolduc10@users.noreply.github.com>
2021-02-25 01:12:33 -0600

    fix

--- GroupProject1_CBZE/src/Driver.java
+++ GroupProject1_CBZE/src/Driver.java
@@ -3,8 +3,6 @@ public class Driver {
 	public static void main(String args[]) {
 		System.out.println("hey");
 		System.out.println("Sup!");
-=======
 		System.out.println("Hello, World1!");
->>>>>>> branch 'master' of https://github.com/CBolduc10/GroupProject1
 	}
 }
