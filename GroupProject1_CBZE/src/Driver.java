dfff1068df8c445c184c0c6e001f883616bec6df
zbolingg <79554953+zbolingg@users.noreply.github.com>
2021-02-25 01:29:09 -0600

    Update Driver.java

--- GroupProject1_CBZE/src/Driver.java
+++ GroupProject1_CBZE/src/Driver.java
@@ -1,6 +1,10 @@
 
 public class Driver {
 	public static void main(String args[]) {
+		System.out.println("This may be sufficient for us to get some work done on these classes");
+		System.out.println("We could figure out how to push/pull from the IDE later on if need be");
+		System.out.println("Until then, we could all just copy-paste via our own project file/classes");
+		System.out.println("It's not pretty, but it could work lol");
 		System.out.println("hey");
 		System.out.println("Sup!");
 		System.out.println("Hello, World1!");
