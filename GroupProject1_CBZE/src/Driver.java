f01975515a9fece91ad3d3975ea17d505319c7e5
Xenokiller <colinbolduc@yahoo.com>
2021-02-26 09:49:22 -0600

    d

--- GroupProject1_CBZE/src/Driver.java
+++ GroupProject1_CBZE/src/Driver.java
@@ -1,8 +1,6 @@
 
 public class Driver {
 	public static void main(String args[]) {
-		System.out.println("A");
-		System.out.println("B");
 		System.out.println("Hello, World1!");
 		System.out.println("cd");
 	}
